a9dd16bd6b2ca746f77f0c09bc68b409833e52c5
Pen9uin <sirpen9uin@gmail.com>
2025-01-11 14:55:45 +0300

    update

--- src/posts.js
+++ src/posts.js
@@ -1,7 +1,9 @@
 import reactLogo from "./assets/react.svg";
 
+const VITE_BACKEND_URL='https://ra-16-router-crud-back.onrender.com'
+
 export async function createPost(payload) {
-  await fetch(import.meta.env.VITE_BACKEND_URL + "/posts", {
+  await fetch(VITE_BACKEND_URL + "/posts", {
     body: JSON.stringify(payload),
     headers: { "Content-Type": "application/json" },
     method: "POST",
@@ -9,25 +11,25 @@ export async function createPost(payload) {
 }
 
 export async function deletePost(id) {
-  await fetch(import.meta.env.VITE_BACKEND_URL + `/posts/${id}`, { method: "DELETE" });
+  await fetch(VITE_BACKEND_URL + `/posts/${id}`, { method: "DELETE" });
 }
 
 export async function getPost(id) {
-  const response = await fetch(import.meta.env.VITE_BACKEND_URL + `/posts/${id}`, { method: "GET" });
+  const response = await fetch(VITE_BACKEND_URL + `/posts/${id}`, { method: "GET" });
   const data = await response.json();
 
   return view(data.post);
 }
 
 export async function getPosts() {
-  const response = await fetch(import.meta.env.VITE_BACKEND_URL + "/posts", { method: "GET" });
+  const response = await fetch(VITE_BACKEND_URL + "/posts", { method: "GET" });
   const data = await response.json();
 
   return data.map(view).sort((p1, p2) => p2.created - p1.created);
 }
 
 export async function updatePost(id, payload) {
-  await fetch(import.meta.env.VITE_BACKEND_URL + `/posts/${id}`, {
+  await fetch(VITE_BACKEND_URL + `/posts/${id}`, {
     body: JSON.stringify(payload),
     headers: { "Content-Type": "application/json" },
     method: "PUT",
